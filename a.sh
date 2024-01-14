697fe6401491d874daa91d2171cdbfbb222a2479
Sombit Pramanik <m.sombitpramanik@gmail.com>
2024-01-14 06:09:16 +0530

    Final Version --v1

---
+++ a.sh
@@ -0,0 +1,7 @@
+echo "# auto-footer" >> README.md
+git init
+git add .
+git commit -m "Final Version --v1"
+git branch -M main
+git remote add origin https://github.com/Sombitpramanik/auto-footer.git
+git push -u origin main
